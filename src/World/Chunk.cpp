84c3d8324318b7a7f0201f46938de3dd0c87a857
Renzo Romeo <renzoromeo44@gmail.com>
2022-06-22 19:10:58 -0300

    dynamic draw

--- src/World/Chunk.cpp
+++ src/World/Chunk.cpp
@@ -132,10 +132,9 @@ namespace MinecraftClone
 		}
 
 		glBindVertexArray(vao);
-
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * cube_vertices.size(), cube_vertices.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * cube_vertices.size(), cube_vertices.data(), GL_DYNAMIC_DRAW);
 
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, position));
 		glEnableVertexAttribArray(0);
